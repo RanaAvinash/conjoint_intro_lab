30e99473c2f7cbb16c08d69f413665e12bbe1def
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 10:59:17 +0530

    Update market_simulator.py

--- modules/market_simulator.py
+++ modules/market_simulator.py
@@ -1,1 +1,30 @@
+import numpy as np
+import pandas as pd
 
+def simulate_market(products, utilities):
+
+    scores = []
+
+    for i,row in products.iterrows():
+
+        u = 0
+
+        for feature,value in row.items():
+
+            key = f"{feature}_{value}"
+
+            if key in utilities["Feature"].values:
+
+                u += utilities.loc[
+                    utilities["Feature"]==key,"Utility"
+                ].values[0]
+
+        scores.append(np.exp(u))
+
+    total = sum(scores)
+
+    shares = [s/total for s in scores]
+
+    products["MarketShare"] = shares
+
+    return products
