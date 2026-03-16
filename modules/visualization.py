2091b092808ef3434989d1f43a5ad0edec58f76d
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 10:59:34 +0530

    Update visualization.py

--- modules/visualization.py
+++ modules/visualization.py
@@ -1,1 +1,12 @@
+import plotly.express as px
 
+def plot_utilities(utilities):
+
+    fig = px.bar(
+        utilities,
+        x="Feature",
+        y="Utility",
+        title="Part-worth Utilities"
+    )
+
+    return fig
