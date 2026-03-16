2098826a8c5ecb1cf8b1d16cf134240c662c3e5e
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 10:58:41 +0530

    Update cbc_conjoint.py

--- modules/cbc_conjoint.py
+++ modules/cbc_conjoint.py
@@ -1,1 +1,25 @@
 
+import numpy as np
+import pandas as pd
+
+def simulate_choices(profiles, n_resp=100):
+
+    data = []
+
+    for r in range(n_resp):
+
+        for i,row in profiles.iterrows():
+
+            utility = np.random.normal()
+
+            prob = np.exp(utility)/(1+np.exp(utility))
+
+            choice = np.random.binomial(1,prob)
+
+            data.append({
+                "Respondent":r,
+                "Profile":row["Profile"],
+                "Choice":choice
+            })
+
+    return pd.DataFrame(data)
