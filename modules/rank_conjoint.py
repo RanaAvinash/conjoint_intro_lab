8ce7328339d5d4d41de34e1c1d4681b5d6f90532
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 10:58:25 +0530

    Update rank_conjoint.py

--- modules/rank_conjoint.py
+++ modules/rank_conjoint.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import statsmodels.api as sm
 
+def estimate_rank_utilities(rank_data, design):
+
+    data = design.copy()
+    data["Rank"] = rank_data
+
+    X = pd.get_dummies(
+        data.drop(columns=["Profile","Rank"]),
+        drop_first=True
+    )
+
+    y = data["Rank"]
+
+    model = sm.OLS(y, sm.add_constant(X))
+    result = model.fit()
+
+    utilities = pd.DataFrame({
+        "Feature": X.columns,
+        "Utility": result.params[1:]
+    })
+
+    return utilities
