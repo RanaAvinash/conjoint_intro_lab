58d26e53a613a233b90a00ad15d52986695e7c64
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 11:18:42 +0530

    Update design_generator.py

--- modules/design_generator.py
+++ modules/design_generator.py
@@ -1,16 +1,15 @@
 import itertools
 import pandas as pd
-from itertools import product
-def generate_profiles(attributes: dict) -> pd.DataFrame:
-    """
-    Generate full factorial profiles
-    """
-    keys = list(attributes.keys())
-    values = list(attributes.values())
 
-    combinations = list(itertools.product(*values))
+def generate_profiles(attributes):
 
-    df = pd.DataFrame(combinations, columns=keys)
-    df["Profile"] = range(1, len(df)+1)
-    combinations = list(product(*values))
+    keys = attributes.keys()
+    values = attributes.values()
+
+    combos = list(itertools.product(*values))
+
+    df = pd.DataFrame(combos,columns=keys)
+
+    df["Profile"] = range(1,len(df)+1)
+
     return df
