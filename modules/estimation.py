c4a809967eeaa402d18858673ad5d815cf407e3b
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-16 10:59:02 +0530

    Update estimation.py

--- modules/estimation.py
+++ modules/estimation.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import statsmodels.api as sm
 
+def estimate_mnl(data, profiles):
+
+    merged = data.merge(profiles,on="Profile")
+
+    X = pd.get_dummies(
+        merged.drop(columns=["Respondent","Choice","Profile"]),
+        drop_first=True
+    )
+
+    y = merged["Choice"]
+
+    model = sm.Logit(y, sm.add_constant(X))
+    result = model.fit()
+
+    utilities = pd.DataFrame({
+        "Feature":X.columns,
+        "Utility":result.params[1:]
+    })
+
+    return utilities
